e40a3fca34208a626ef930e3ec20647aa6d0246a
supreet <65367030+Supreet984@users.noreply.github.com>
2023-04-21 18:19:03 +0530

    added tests and fixed some bugs

--- src/main/java/com/fooddelivery/fooddelivery/FooddeliveryApplication.java
+++ src/main/java/com/fooddelivery/fooddelivery/FooddeliveryApplication.java
@@ -7,7 +7,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = {"com.fooddelivery.fooddelivery.controllers", "com.fooddelivery.fooddelivery.services", "com.fooddelivery.fooddelivery.repositories"})
 public class FooddeliveryApplication {
 
     public static void main(String[] args) {
